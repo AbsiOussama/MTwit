623b701de5fab4d0cce160cd3f9dabf5625037cc
oussama absi <oussamaabsi007@gmail.com>
2013-05-06 16:22:46 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MTwit::Application.config.secret_token = '62370eafdc5171c8e93e7673d1f98d3dcd0b7a3855303884dd545944fe52b5933b343bf49468d301f9090c087b336627563d0c902dad8510384ebab8780ee946'
